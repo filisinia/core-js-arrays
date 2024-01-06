431fbe427d81b66177790a1479ed9f55117fab3b
Mariia Petrenko <filisinia@gmail.com>
2024-01-06 12:22:45 +0200

    feat: solve shiftArray

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -564,8 +564,14 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  let toShift = [];
+  if (n >= 0) {
+    toShift = arr.splice(0, n + 1);
+  } else {
+    toShift = arr.splice(0, Math.abs(n));
+  }
+  return [...arr, ...toShift];
 }
 
 /**
