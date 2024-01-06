7885c35dc04bf459f575fe228fbac9e29fce5199
Mariia Petrenko <filisinia@gmail.com>
2024-01-06 12:01:27 +0200

    feat: solve sortDigitNamesByNumericOrder

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -551,8 +551,6 @@ function propagateItemsByPositionIndex(arr) {
   return res.flat();
 }
 
-// console.log(propagateItemsByPositionIndex([ 'a', 'b', 'c', null ]));
-
 /**
  * Shifts an array by n positions. If n is negative, the array is shifted to the left;
  * if positive, it is shifted to the right.
@@ -583,8 +581,31 @@ function shiftArray(/* arr, n */) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const digits = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+  const digitsArr = [];
+
+  arr.map((elem) => {
+    digitsArr.push(digits[elem]);
+    return elem;
+  });
+
+  digitsArr.sort((a, b) => a - b);
+
+  return digitsArr.map((elem) => {
+    return Object.keys(digits).find((key) => digits[key] === elem);
+  });
 }
 
 /**
